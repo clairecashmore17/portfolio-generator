1fff9a947e1cda59003f48ac42cc1b548494e575
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-07 10:24:52 -0400

    dynamically created an HTML page with node.js

--- app.js
+++ app.js
@@ -1,17 +1,18 @@
+//necessarry fs define line!
+//"require" saves core module to fs that makes it a special object that has access to all the fs information from node.js
+const fs = require('fs');
+
+//Require statement to import our page-template js
+const generatePage = require('./src/page-template.js');
+
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-// console.log(profileDataArgs);
-
-//function to print the profile data to the console.
-const printProfileData = profileDataArr => {
-    for(let i =0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==================');
-
-    //is the same as this...
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const name = profileDataArgs[0];
+// const github  =profileDataArgs[1];
+// Is the same as...
+const [name, github] = profileDataArgs;
 
 
-
-printProfileData(profileDataArgs);
+fs.writeFile('./index.html', generatePage(name,github), err => {
+    if(err) throw new Error(err);
+    console.log("Portfolio Complete! Checkout index.html to see the output!")
+});

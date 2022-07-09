cf96808e2d89c83a41eff67cc1ea329760044f78
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-08 23:08:12 -0400

    added inquirer to project

--- app.js
+++ app.js
@@ -1,18 +1,26 @@
-//necessarry fs define line!
-//"require" saves core module to fs that makes it a special object that has access to all the fs information from node.js
-const fs = require('fs');
+// import inquirer package into our project
+const inquirer = require('inquirer');
 
-//Require statement to import our page-template js
-const generatePage = require('./src/page-template.js');
+inquirer
+    .prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        }
+    ])
+    .then(answers => console.log(answers));
+// //necessarry fs define line!
+// //"require" saves core module to fs that makes it a special object that has access to all the fs information from node.js
+// const fs = require('fs');
 
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-// const name = profileDataArgs[0];
-// const github  =profileDataArgs[1];
-// Is the same as...
-const [name, github] = profileDataArgs;
+// //Require statement to import our page-template js
+// const generatePage = require('./src/page-template.js');
 
 
-fs.writeFile('./index.html', generatePage(name,github), err => {
-    if(err) throw new Error(err);
-    console.log("Portfolio Complete! Checkout index.html to see the output!")
-});
+
+
+// fs.writeFile('./index.html', generatePage(name,github), err => {
+//     if(err) throw new Error(err);
+//     console.log("Portfolio Complete! Checkout index.html to see the output!")
+// });

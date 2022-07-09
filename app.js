f0710d0d0d95372e24a5e1f1622aadfe4fe73c28
Claire Cashmore <clairecashmore2017@gmail.com>
2022-07-09 00:30:47 -0400

    added validates and when for required info

--- app.js
+++ app.js
@@ -7,7 +7,16 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                }
+                else{
+                    console.log('Please enter your name!');
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
@@ -15,10 +24,26 @@ const promptUser = () => {
             message: 'Enter your Github Username'
         },
         {
+            type: 'confirm',
+            name: 'confirmAbout',
+            message: 'Would you like to enter some information about yourself for an "About" section',
+            default: true
+        },
+        {
             type: 'input',
             name: 'about',
-            message: 'Provide some information about yourself:'
+            message: 'Provide some information about yourself:',
+            when: ({ confirmAbout }) => {
+                if (confirmAbout) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            }
         }
+       
+
     ]);
 };
 
@@ -37,12 +62,30 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is the name of your project?'
+            message: 'What is the name of your project? (Required) ',
+            validate: projectName => {
+                if(projectName){
+                    return true;
+                }
+                else {
+                    console.log("Please provide a project name!");
+                    return false;
+                }
+            }
         },
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required)',
+            validate: projectDescription => {
+                if(projectDescription) {
+                    return true;
+                }
+                else {
+                    console.log("Please add a description!");
+                    return false;
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -53,7 +96,16 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your project. (Required)'
+            message: 'Enter the Github link to your project. (Required)',
+            validate: gitLink => {
+                if(gitLink){
+                    return true;
+                }
+                else {
+                    console.log("Please provide a link!");
+                    return false;
+                }
+            }
         },
         {
             type: 'confirm',
